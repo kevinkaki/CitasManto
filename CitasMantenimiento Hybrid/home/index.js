56b438d545a68c94b7f3f6a3e01f7a687d3d9253
Kevin Kakiyama Vergara <kevinkaki@hotmail.com>
2015-05-12 21:08:40 +0000

    Changes from Screen Builder

--- CitasMantenimiento Hybrid/home/index.js
+++ CitasMantenimiento Hybrid/home/index.js
@@ -3,3 +3,16 @@
 app.home = kendo.observable({
     onShow: function() {}
 });
+(function(parent) {
+    var homeModel = kendo.observable({
+        openLink: function(url) {
+            window.open(url, '_system');
+            if (window.event) {
+                window.event.preventDefault && window.event.preventDefault();
+                window.event.returnValue = false;
+            }
+        }
+    });
+
+    parent.set('homeModel', homeModel);
+})(app.home);
