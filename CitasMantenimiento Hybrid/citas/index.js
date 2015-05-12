4b8a5f09afb107a6972ce271d1c9a860ceb0f2e9
Kevin Kakiyama Vergara <kevinkaki@hotmail.com>
2015-05-12 19:55:43 +0000

    Changes from Screen Builder

--- CitasMantenimiento Hybrid/citas/index.js
+++ CitasMantenimiento Hybrid/citas/index.js
@@ -4,44 +4,6 @@ app.citas = kendo.observable({
     onShow: function() {}
 });
 (function(parent) {
-    /* var provider = app.data.citasMantenimientoBackend,
-       
-        signupSuccess =
-        function(data) {
-            app.user = data.result;
-            app.mobileApp.navigate('home/view.html');
-        },
-        
-        signupInit =
-        function() {
-            if (provider.setup.offlineStorage && !app.isOnline()) {
-                $('.signup-view').hide().siblings().show();
-            } else {
-                $('.signup-view').show().siblings().hide();
-            }
-        },
-        
-        registerViewModel = kendo.observable({
-            username: '',
-            password: '',
-            email: '',
-            register: function() {
-                var attrs = {
-                    Email: registerViewModel.email,
-                    
-                };
-
-                provider.Users.register(registerViewModel.username, registerViewModel.password, attrs,
-                    function(data) {
-                        if (data && data.result) {
-                            signupSuccess(data);
-                        }
-                    },
-                    signupInit
-                );
-            }
-        });*/
-    
     var citasModel = kendo.observable({
         fields: {
             sucursal: '',
@@ -49,14 +11,8 @@ app.citas = kendo.observable({
             placa: '',
             date: '',
         },
-        submit: function() {
-                
-            app.mobileApp.navigate('home/view.html');
-        },
-        cancel: function() {
-            
-            app.mobileApp.navigate('home/view.html');
-        }
+        submit: function() {},
+        cancel: function() {}
     });
 
     parent.set('citasModel', citasModel);
